14109d27e2bdc226107d622998a66e23c3a819d7
Vinicius Vilela <viniciusvilela19@gmail.com>
2024-02-02 15:29:34 +0000

    feat: injection token

--- apps/angular/injection-token/src/app/data.ts
+++ apps/angular/injection-token/src/app/data.ts
@@ -1,1 +1,8 @@
-export const DEFAULT_TIMER = 1000;
+import { InjectionToken } from '@angular/core';
+
+export const DEFAULT_TIMER = new InjectionToken<number>('DEFAULT_TIMER');
+
+export const getDefaultTimerProvider = (value?: number) => ({
+  provide: DEFAULT_TIMER,
+  useValue: value ?? 1000,
+});
